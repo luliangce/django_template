b03c5295427f387a21f2ba9afdd9df147d73bc19
L.Lu <luliangce@gmail.com>
2020-04-21 17:53:20 +0800

    简化错误码的调用方式

--- ecode/basic.py
+++ ecode/basic.py
@@ -10,13 +10,20 @@ class E:
         self.code = code
         self.msg = msg
 
-    def inherit(self, msg: str) -> "E":
-        """根据现有模板,变更返回信息之后生成一个新的模板  """
-        return E(self.code, msg=msg)
+    def inherit(self, msg: str, *args, **kwargs) -> "E":
+        """ 根据现有模板,变更返回信息之后生成一个新的模板
+            msg支持使用format方式传递参数
+            >>> OK('a={}',1) # 等价于 OK.inherit('a={}'.format(1))                                        
+        """
+        return E(self.code, msg=msg.format(*args, **kwargs))
+
+    __call__ = inherit
+
+    def __str__(self):
+        return f'{self.code}:{self.msg}'
+
+    __repr__ = __str__
 
     @property
     def body(self):
-        return dict(
-            code=self.code,
-            msg=self.msg
-        )
+        return dict(code=self.code, msg=self.msg)
