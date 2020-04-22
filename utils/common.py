64f9c3fd7ed4a7c494c5a5eb1814e733f97473bf
L.Lu <luliangce@gmail.com>
2020-04-22 10:37:46 +0800

    增加两个工具函数

--- utils/common.py
+++ utils/common.py
@@ -1,8 +1,9 @@
+import contextlib
 import json
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Union, List, Tuple, Type
 
 from django.conf import settings
-from django.http.response import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from jose import jwt
 
 from ecode import OK, E
@@ -23,6 +24,28 @@ def JSON(code: E = OK, data: Union[dict] = None, **kwargs) -> HttpResponse:
                         **kwargs)
 
 
-
 def issue_token(user: 'User') -> str:
     return jwt.encode({"id": user.id}, key=settings.SECRET_KEY)
+
+
+@contextlib.contextmanager
+def dontcare(exception=Exception):
+    """
+    通过上下文管理器捕获异常，减少请求方法中使用try:...except:...的次数
+    >>> with dontcare():
+    >>>     condition['param'] = 1 / 0
+    >>> with dontcare():
+    >>>     condition['user'] = 1
+    """
+    try:
+        yield None
+    except exception:
+        ...
+
+
+def get_lo(request: 'HttpRequest', limit=10, offset=0) -> (int, int):
+    with dontcare():
+        limit = int(request.GET['limit'])
+    with dontcare():
+        offset = int(request.GET['offset'])
+    return offset, limit
